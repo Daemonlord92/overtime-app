b1a83ff55e480a4b57de04195cd677902743cb83
Matthew Jeshua Martin <mmartin@localhost.localdomain>
2018-04-23 21:15:46 -0700

    FactoryGirl installed and setup with test data

--- Gemfile
+++ Gemfile
@@ -22,6 +22,8 @@ group :development, :test do
   gem 'selenium-webdriver'
   gem 'rspec-rails', '~> 3.0'
   gem 'database_cleaner'
+  gem 'factory_girl', '~> 4.9'
+  gem 'factory_girl_rails', '~> 4.9'
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 
-@user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+@user = User.create(email: "user@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
 puts '1 user created'
 
 100.times do |post|
