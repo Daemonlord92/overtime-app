c360babcd01da8a3a77f10826996475d633ba767
Matthew <zhintateo19924559@gmail.com>
2018-06-05 09:22:15 -0700

    showing enum status on the posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,10 @@
 @user = User.create(email: "user@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
 puts '1 user created'
 
+AdminUser.create(email: "admin@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Admin", last_name: "Name")
+puts "1 Admin User created"
+
 100.times do |post|
 	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
-
 puts "100 Posts have been created"
