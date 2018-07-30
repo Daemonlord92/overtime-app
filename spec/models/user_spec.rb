f0143a97a8309b8952ce5fb2728755fbe76a7b19
Matthew <zhintateo19924559@gmail.com>
2018-07-29 20:26:49 -0700

    Updated gritter growl function

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,9 +18,4 @@ RSpec.describe User, type: :model do
     end
 
   end
-  describe "custom name methods" do
-    it 'has a full name method that combines first and last name' do
-      expect(@user.full_name).to eq("SNOW, JON")
-    end
-end
 end
