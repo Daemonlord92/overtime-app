efbac005ef104f544e37b73f7cd340aafc202d0c
3JM <zhintateo19924559@gmail.com>
2018-11-24 13:14:00 -0800

    setup skeleton employee homepage

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,10 @@
 class StaticController < ApplicationController
 	def homepage
-    @pending_approvals = Post.where(status: 'submitted')
-    @recent_audit_items = AuditLog.last(10)
+    if current_user.type == 'AdminUser'
+      @pending_approvals = Post.submitted
+      @recent_audit_items = AuditLog.last(10)
+    else
+      #something
+    end
 	end
-
 end
